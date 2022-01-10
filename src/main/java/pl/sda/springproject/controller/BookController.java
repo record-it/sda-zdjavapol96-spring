c604696a3f3752ee6ba3f4076cfe9ca7e05fb39b
cs <siwon.cezary@gmail.com>
2022-01-10 17:19:13 +0100

    day 8

--- src/main/java/pl/sda/springproject/controller/BookController.java
+++ src/main/java/pl/sda/springproject/controller/BookController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import pl.sda.springproject.dto.BookDto;
-import pl.sda.springproject.model.Book;
 import pl.sda.springproject.service.BookService;
 
 @Controller
